a9c220bd588ccac01f785b1188db4480b4ae8bdc
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-20 10:50:55 +0700

    setup banner

---
+++ components/Banner.tsx
@@ -0,0 +1,102 @@
+"use client";
+import React, { useState } from "react";
+import { BackgroundBeams } from "./BackgroundBeams";
+
+export default function Banner({ changeWord }: { changeWord: any }) {
+  const [value, setValue] = useState("");
+
+  return (
+    // <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%  h-[40vh]">
+    //   <form className="max-w-md mx-auto">
+    //     <label htmlFor="default-search" className="mb-2  text-sm font-medium text-gray-900 sr-only dark:text-white">
+    //       Search
+    //     </label>
+    //     <div className="relative top-[120px]">
+    //       <div className="absolute top inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+    //         <svg
+    //           className="w-4 h-4 text-gray-500 dark:text-gray-400"
+    //           aria-hidden="true"
+    //           xmlns="http://www.w3.org/2000/svg"
+    //           fill="none"
+    //           viewBox="0 0 20 20"
+    //         >
+    //           <path
+    //             stroke="currentColor"
+    //             strokeLinecap="round"
+    //             strokeLinejoin="round"
+    //             strokeWidth="2"
+    //             d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+    //           />
+    //         </svg>
+    //       </div>
+    //       <input
+    //         type="search"
+    //         id="default-search"
+    //         className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-white"
+    //         placeholder="Search Mockups, Logos..."
+    //         onChange={(e) => {
+    //           setValue(e.target.value);
+    //         }}
+    //         required
+    //       />
+    //       <button
+    //         type="button"
+    //         onClick={() => {
+    //           changeWord(value);
+    //         }}
+    //         className="text-white absolute end-2.5 bottom-2 bg-green-600 hover:bg-green-700   px-5 py-2 rounded-full mt-2 "
+    //       >
+    //         Search
+    //       </button>
+    //     </div>
+    //   </form>
+    // </div>
+
+    <div className="h-[40rem] w-full bg-neutral-950 relative flex flex-col items-center justify-center ">
+      <BackgroundBeams />
+      <form className="w-2/5">
+        <label htmlFor="default-search" className="mb-2  text-sm font-medium text-gray-900 sr-only dark:text-white">
+          Search
+        </label>
+        <div className="relative">
+          <div className="absolute top inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-white"
+            placeholder="Find word..."
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+            required
+          />
+          <button
+            type="button"
+            onClick={() => {
+              changeWord(value);
+            }}
+            className="text-white absolute end-2.5 bottom-2 bg-green-600 hover:bg-green-700   px-5 py-2 rounded-full mt-2 "
+          >
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from "tailwindcss";
 
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+const colors = require("tailwindcss/colors");
+const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,11 +15,21 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default config;
+
